b21439fc7c499a7b009f69e3f0a11e46741b5f7c
yangfengshuai <yangfengshuai@koolearn.com>
2020-03-20 20:18:31 +0800

    for循环使用协程

--- routine-yfs/src/main/kotlin/com/routine/kotlin/plugins/coroutine/CoroutineDemo.kt
+++ routine-yfs/src/main/kotlin/com/routine/kotlin/plugins/coroutine/CoroutineDemo.kt
@@ -1,23 +1,41 @@
 package com.routine.kotlin.plugins.coroutine
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.future.asCompletableFuture
+import java.time.LocalDateTime
 
 
 fun main() {
-    getStr()
+    val list = listOf("a", "b", "c", "d", "e")
+    println("start process" + LocalDateTime.now())
+    val resultList = CoroutineScope(Dispatchers.Default).async {
+        val deferredList = mutableListOf<Deferred<String>>()
+        list.forEach {
+            val str = async { getStr(it) }
+            deferredList.add(str)
+        }
+        deferredList.map { it.await() }
+    }.asCompletableFuture().get()
+    println(resultList + LocalDateTime.now())
+}
+
+
+fun getStr(str: String): String {
+    Thread.sleep(5000)
+    return str + str;
 }
 
 
 fun getStr() {
     println("start process")
     CoroutineScope(Dispatchers.Default).async {
-        val i = async {getI()}
-        val am = async {getAm()}
-        val from = async {getFrom()}
-        val city = async {getCity()}
+        val i = async { getI() }
+        val am = async { getAm() }
+        val from = async { getFrom() }
+        val city = async { getCity() }
         val result = "${i.await()} ${am.await()} ${from.await()} ${city.await()}"
         println(result)
     }.asCompletableFuture().get()
@@ -27,6 +45,7 @@ fun getI(): String {
     Thread.sleep(5000)
     return "I "
 }
+
 fun getAm(): String {
     Thread.sleep(5000)
     return "am "
