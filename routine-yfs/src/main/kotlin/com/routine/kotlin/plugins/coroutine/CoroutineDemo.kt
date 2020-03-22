b31ce9377805151c4e3fc88b2ec6229899661d0a
yangfengshuai <yangfengshuai@koolearn.com>
2020-03-22 12:38:33 +0800

    协程launch

--- routine-yfs/src/main/kotlin/com/routine/kotlin/plugins/coroutine/CoroutineDemo.kt
+++ routine-yfs/src/main/kotlin/com/routine/kotlin/plugins/coroutine/CoroutineDemo.kt
@@ -7,7 +7,22 @@ import kotlin.system.measureTimeMillis
 
 
 fun main() {
-    launchTest()
+    runBlocking {
+        val launch = launch {
+            // 默认继承 parent coroutine 的 CoroutineDispatcher，指定运行在 main 线程
+            println("main runBlocking: I'm working in thread ${Thread.currentThread().name}")
+            delay(10000)
+            println("main runBlocking: After delay in thread ${Thread.currentThread().name}")
+        }
+        delay(10000)
+        launch.start()
+        launch(Dispatchers.Unconfined) {
+            println("Unconfined      : I'm working in thread ${Thread.currentThread().name}")
+            delay(100)
+            println("Unconfined      : After delay in thread ${Thread.currentThread().name}")
+        }
+    }
+    println(123)
 }
 
 fun launchTest() {
