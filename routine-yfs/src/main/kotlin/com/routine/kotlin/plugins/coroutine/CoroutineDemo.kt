6011623e25327df4f613035a3fce0a7099eaa821
yangfengshuai <yangfengshuai@koolearn.com>
2020-03-22 12:16:20 +0800

    kotlin 协程用法

--- routine-yfs/src/main/kotlin/com/routine/kotlin/plugins/coroutine/CoroutineDemo.kt
+++ routine-yfs/src/main/kotlin/com/routine/kotlin/plugins/coroutine/CoroutineDemo.kt
@@ -2,19 +2,103 @@ package com.routine.kotlin.plugins.coroutine
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.future.asCompletableFuture
-import org.junit.Test
 import java.time.LocalDateTime
 import kotlin.system.measureTimeMillis
 
 
 fun main() {
-    dispatcherTest()
+    launchTest()
+}
+
+fun launchTest() {
+    val list = listOf("aa", "bb", "cc", "bb", "cc", "bb", "cc", "bb", "cc", "dde")
+    // values 的格式是 [aa,aa,aa] [bb,bb,bb] [cc,cc,cc],[dd]
+    val values = list.groupBy { it }.values.toList()
+    CoroutineScope(Dispatchers.IO).async {
+        values.forEach {
+            launch {
+                it.forEach {
+                    delay(5000)
+                    println(it + ",current thread is " + Thread.currentThread().id)
+                }
+            }
+        }
+    }.asCompletableFuture().get()
+    println("end 1----------------------------")
+    values.forEach {
+        CoroutineScope(Dispatchers.IO).launch {
+            it.forEach {
+                delay(5000)
+                println(it + ",current thread is " + Thread.currentThread().id)
+            }
+        }
+    }
+    println("end 2 ----------------------------")
+    Thread.sleep(5000000)
+}
+
+
+fun delayAndForTest() {
+    val list = listOf("aa", "bb", "cc", "bb", "cc", "bb", "cc", "bb", "cc", "dd", "aa", "a", "v", "s", "s", "fads", "hh", "qd", "de", "kk", "aa", "bb", "cc",
+            "bb", "cc", "bb", "cc", "bb", "cc", "dd", "aa", "a", "v", "s", "s", "fads", "hh", "qd", "de", "kk",
+            "bab", "cca", "abb", "acc", "abb", "cac", "dad", "aaa", "aaf", "fv", "sf", "sf", "hello", "hfh", "qdf", "def", "kkf",
+            "bb", "cc", "bb", "cc", "bb", "cc", "dd", "aa", "a", "v", "s", "s", "fads", "hh", "qd", "de", "kk"
+    )
+    // values 的格式是 [aa,aa,aa] [bb,bb,bb] [cc,cc,cc],[dd]
+    val values = list.groupBy { it }.values.toList()
+    handleLists(values)
+}
+
+
+fun handleFor() {
+    val list = listOf("aa", "bb", "cc", "bb", "cc", "bb", "cc", "bb", "cc", "dd")
+    // values 的格式是 [aa,aa,aa] [bb,bb,bb] [cc,cc,cc],[dd]
+    val values = list.groupBy { it }.values.toList()
+    CoroutineScope(Dispatchers.IO).async {
+        values.map {
+            async {
+                it.map { it1 ->
+                    it1.forEach {
+                        delay(5000)
+                        println(it + ",current thread is " + Thread.currentThread().id)
+                    }
+                }
+            }
+        }.forEach { it.await() }
+    }.asCompletableFuture().get()
+
+
+}
+
+private fun handleListList(list: List<String>) {
+    CoroutineScope(Dispatchers.IO).async {
+        list.forEach {
+            delay(5000)
+            println(it)
+        }
+    }.asCompletableFuture().get()
+}
+
+private fun handleLists(lists: List<List<String>>) {
+    CoroutineScope(Dispatchers.IO).async {
+        lists.map {
+            async {
+                it.forEach {
+                    delay(5000)
+                    println(it + ",current thread is " + Thread.currentThread().id)
+                }
+            }
+        }.map {
+            it.await()
+        }
+    }.asCompletableFuture().get()
 }
 
 
 fun dispatcherTest() {
     runBlocking {
-        launch { // 默认继承 parent coroutine 的 CoroutineDispatcher，指定运行在 main 线程
+        launch {
+            // 默认继承 parent coroutine 的 CoroutineDispatcher，指定运行在 main 线程
             println("main runBlocking: I'm working in thread ${Thread.currentThread().name}")
             delay(100)
             println("main runBlocking: After delay in thread ${Thread.currentThread().name}")
@@ -40,7 +124,6 @@ fun asyncTest() {
         println(measureTimeMillis)
     }
 }
-
 
 
 fun coroutineScopeTest() {
