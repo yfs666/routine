ef731e4f83d574d2ad5b186506b02f85d8d2cc56
yangfengshuai <416204871@qq.com>
2020-10-12 21:03:41 +0800

    练习

---
+++ routine-yfs/src/main/java/com/routine/java/jvm/bytecode/MyTest1.java
@@ -0,0 +1,21 @@
+package com.routine.java.jvm.bytecode;
+
+public class MyTest1 {
+    private int a = 1;
+
+    public MyTest1(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
