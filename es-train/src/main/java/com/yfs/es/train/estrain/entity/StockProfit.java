7fe889b8254eb0e331f11ffa7ca37313d2a4ee9a
yangfengshuai <416204871@qq.com>
2021-01-10 11:03:17 +0800

    收益

---
+++ es-train/src/main/java/com/yfs/es/train/estrain/entity/StockProfit.java
@@ -0,0 +1,13 @@
+package com.yfs.es.train.estrain.entity;
+
+import java.math.BigDecimal;
+
+public class StockProfit {
+    private String symbol;
+    private Long reportDate;
+    private String reportName;
+    private Long profit;
+    private BigDecimal profitPercent;
+    private long revenue;
+    private BigDecimal revenuePercent;
+}
